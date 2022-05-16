567947b3a7f9e9b6a94db957e10dd8d668a743e9
frank.wu <frank.wu@imile.me>
2022-05-16 16:20:46 +0800

    feat: 抽离独立出redux文件

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,5 @@
-import React, { useState, useContext, useMemo, useEffect } from "react";
-
-const AppContext = React.createContext(null);
-const store = {
-  state: {
-    user: { name: "frank", age: 18 },
-  },
-  setState: (newState) => {
-    store.state = newState;
-    // 更新所有订阅的组件
-    store.listeners.map((fn) => fn(store.state));
-  },
-  listeners: [],
-  // 订阅
-  subscribe: (fn) => {
-    store.listeners.push(fn);
-    // 取消订阅
-    return () => {
-      const index = store.listeners.indexOf(fn);
-      store.listeners.splice(index, 1);
-    };
-  },
-};
-
-const connect = (Component) => {
-  const Wrapper = (props) => {
-    const store = useContext(AppContext);
-    const { state, setState, subscribe } = store;
-    // 订阅 & 更新
-    const [, update] = useState({});
-    useEffect(() => {
-      subscribe(() => {
-        update({});
-      });
-    }, []);
-    // 更新数据
-    const dispatch = (action) => {
-      const newState = reducer(state, action);
-      setState(newState);
-    };
-    return <Component {...props} dispatch={dispatch} state={state} />;
-  };
-  return Wrapper;
-};
+import React from "react";
+import { AppContext, connect, store } from "./redux";
 
 const App = () => {
   return (
@@ -80,24 +38,8 @@ const ThirdSon = () => {
 
 // 读数据
 const User = connect(({ state }) => {
-  console.log9;
   return <div>User:{state.user.name}</div>;
 });
-
-// 规范创建新的state过程: reducer(creatNewState)
-const reducer = (state, action) => {
-  if (action.type === "updateUser") {
-    return {
-      ...state,
-      user: {
-        ...state.user,
-        ...action.data,
-      },
-    };
-  } else {
-    return state;
-  }
-};
 
 // createWrapper 将组件和全局的state链接起来
 const UserModifier = connect(({ state, dispatch, children }) => {
---
+++ src/redux.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useContext, useMemo, useEffect } from "react";
+
+export const store = {
+  state: {
+    user: { name: "frank", age: 18 },
+  },
+  setState: (newState) => {
+    store.state = newState;
+    // 更新所有订阅的组件
+    store.listeners.map((fn) => fn(store.state));
+  },
+  listeners: [],
+  // 订阅
+  subscribe: (fn) => {
+    store.listeners.push(fn);
+    // 取消订阅
+    return () => {
+      const index = store.listeners.indexOf(fn);
+      store.listeners.splice(index, 1);
+    };
+  },
+};
+
+// 规范创建新的state过程: reducer(creatNewState)
+const reducer = (state, action) => {
+  if (action.type === "updateUser") {
+    return {
+      ...state,
+      user: {
+        ...state.user,
+        ...action.data,
+      },
+    };
+  } else {
+    return state;
+  }
+};
+
+export const AppContext = React.createContext(null);
+
+export const connect = (Component) => {
+  const Wrapper = (props) => {
+    const store = useContext(AppContext);
+    const { state, setState, subscribe } = store;
+    // 订阅 & 更新
+    const [, update] = useState({});
+    useEffect(() => {
+      subscribe(() => {
+        update({});
+      });
+    }, []);
+    // 更新数据
+    const dispatch = (action) => {
+      const newState = reducer(state, action);
+      setState(newState);
+    };
+    return <Component {...props} dispatch={dispatch} state={state} />;
+  };
+  return Wrapper;
+};
